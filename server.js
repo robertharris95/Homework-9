82e947a28798725c2556c07da7e694eac1d4c454
Robert Harris <rlh9@rice.edu>
2020-03-06 23:02:58 -0600

    deletes all

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get("/notes", function(req, res) {
 app.get("/api/notes", function(req, res) {
     fs.readFile('./db/db.json','utf-8',function(err, data){
         if (err) throw err;
+        console.log(data);
         return res.json(JSON.parse(data));
     })
 });
@@ -43,22 +44,25 @@ app.post("/api/notes", function(req, res) {
 //deletes a note when clicked
 app.delete("/api/notes/:id", function(req, res){
     var target = req.params.id
-    fs.readFile('./db/db.json','utf-8',function(err, data){
-        if (err) throw err;
-        var object = JSON.parse(data)
-        for(let i = 0; i<target; i++){
-        var objectId = object[i].id
-        if (target == objectId)
-        {
-            notes.splice(i,1);
-            fs.writeFile('./db/db.json',`[${notes}]`,"utf-8",function(err){
+        fs.readFile('./db/db.json', 'utf8', function(err, data){
+            if (err) {
+            throw err;
+            }
+            let object = JSON.parse(data);
+            for (note in object) {
+                if (object[note].id === target) {
+                    object.splice(note, 1);
+                fs.writeFile("./db/db.json", `[${notes}]`, "utf-8", function(err){
                 if (err) throw err;
-                return res.json(req.body)});
-        }
-    }
-    })
-    
+                return;
+                });
+                }
+                };
+                res.end();
+    });
 });
+
+
 
 //catch all for HTML at incorrect locatons
 app.get("*", function(req, res) {
